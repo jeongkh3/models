698f845284b5f0822839189b96d094d9806f4f0f
asher.chung <asher.chung@kakaobank.com>
2018-02-02 13:49:23 +0900

    modified fontdatset_eval

--- research/object_detection/fontdataset_eval.sh
+++ research/object_detection/fontdataset_eval.sh
@@ -5,8 +5,9 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 
-python eval.py \
+python eval_fontdatset.py \
 --logtostderr \
 --pipeline_config=/Users/asher/dev/workspace/exercise/test_models/model/ssd_inception_v2_fontdataset.config \
---checkpoint_dir=/Users/asher/dev/workspace/exercise/test_models/model/train
---eval_dir=/Users/asher/dev/workspace/exercise/test_models/model/eval
+--checkpoint_dir=/Users/asher/dev/workspace/exercise/test_models/model/train \
+--eval_dir=/Users/asher/dev/workspace/exercise/test_models/model/eval \
+--data_dir=/Users/asher/dev/workspace/exercise/rcnn/tf-faster-rcnn/data/fontdataset
